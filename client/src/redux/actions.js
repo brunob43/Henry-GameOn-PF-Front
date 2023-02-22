ea2730286b0fbe3c2dfbde4cfb9d660f1999541f
FrancoFrias23 <lockke2323@gmail.com>
2023-02-21 21:20:08 -0300

    ..

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -262,3 +262,9 @@ export function payment100 (){
   } 
 }
 
+export function postMessage (input){
+  return async function (){
+    const response = await axios.post('/message',input);
+   return response;
+  }
+}
