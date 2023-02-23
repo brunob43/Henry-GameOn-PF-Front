65b2adea62d116e14615de74418934cae0f68b6d
NicoleA4 <nicolea413@gmail.com>
2023-02-23 16:54:58 -0300

    quitar persist

--- client/src/index.js
+++ client/src/index.js
@@ -6,26 +6,26 @@ import store from './redux/store';
 import App from './App';
 import './index.css';
 import {Auth0Provider} from "@auth0/auth0-react";
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/integration/react'
+// import { persistStore } from 'redux-persist';
 import {ChakraProvider} from "@chakra-ui/react"
-import { Spinner } from '@chakra-ui/react'
+// import { Spinner } from '@chakra-ui/react'
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const persistor = persistStore(store)
+// const persistor = persistStore(store)
 
 ReactDOM.render(
   <ChakraProvider>
-  <PersistGate loading={
+  {/* <PersistGate loading={
   <Spinner
   thickness='4px'
   speed='0.65s'
   emptyColor='gray.200'
   color='blue.500'
   size='xl'
-  />} persistor={persistor}>
+  />} persistor={persistor}> */}
     <Provider store={store}>
       <Auth0Provider
       domain="henrygameon.eu.auth0.com"
@@ -39,7 +39,7 @@ ReactDOM.render(
         </BrowserRouter>      
       </Auth0Provider>
     </Provider>
-  </PersistGate>
+  {/* </PersistGate> */}
   </ChakraProvider>,
   document.getElementById('root')
 );
