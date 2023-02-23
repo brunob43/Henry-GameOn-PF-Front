65130e29bc876e2e2249b7d834295af99d70ec86
NicoleA4 <nicolea413@gmail.com>
2023-02-23 14:27:28 -0300

    persist

--- client/src/index.js
+++ client/src/index.js
@@ -6,23 +6,40 @@ import store from './redux/store';
 import App from './App';
 import './index.css';
 import {Auth0Provider} from "@auth0/auth0-react";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
+import {ChakraProvider} from "@chakra-ui/react"
+import { Spinner } from '@chakra-ui/react'
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+const persistor = persistStore(store)
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Auth0Provider
-    domain="henrygameon.eu.auth0.com"
-    clientId="I8Pua9FRXYxAahGm0gH1UJonTh1Xspjc"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-     <BrowserRouter>
-        <App />
-     </BrowserRouter>
-    </Auth0Provider>
-  </Provider>,
+  <ChakraProvider>
+  <PersistGate loading={
+  <Spinner
+  thickness='4px'
+  speed='0.65s'
+  emptyColor='gray.200'
+  color='blue.500'
+  size='xl'
+  />} persistor={persistor}>
+    <Provider store={store}>
+      <Auth0Provider
+      domain="henrygameon.eu.auth0.com"
+      clientId="I8Pua9FRXYxAahGm0gH1UJonTh1Xspjc"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>      
+      </Auth0Provider>
+    </Provider>
+  </PersistGate>
+  </ChakraProvider>,
   document.getElementById('root')
 );
