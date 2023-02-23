bfe5b18ad9d6f8b1b28b9618e0e98e291aecd490
LarryJose <larryjoseguarenas@gmail.com>
2023-02-23 20:54:00 -0300

    act home y about chakra

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './redux/store';
-import App from './App';
-import './index.css';
-import {Auth0Provider} from "@auth0/auth0-react";
-
+import store from "./redux/store";
+import App from "./App";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { ChakraProvider } from "@chakra-ui/react";
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Auth0Provider
-    domain="henrygameon.eu.auth0.com"
-    clientId="I8Pua9FRXYxAahGm0gH1UJonTh1Xspjc"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-     <BrowserRouter>
-        <App />
-     </BrowserRouter>
-    </Auth0Provider>
-  </Provider>,
-  document.getElementById('root')
+  <ChakraProvider>
+    <Provider store={store}>
+      <Auth0Provider
+        domain="henrygameon.eu.auth0.com"
+        clientId="I8Pua9FRXYxAahGm0gH1UJonTh1Xspjc"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Auth0Provider>
+    </Provider>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -1,219 +1,271 @@
-import style from "./About.module.css"
-import linkedin from "../assets/imagen/linkedin.png"
-import github from "../assets/imagen/github.png"
+// import style from "./About.module.css";
+import linkedin from "../assets/imagen/linkedin.png";
+import github from "../assets/imagen/github.png";
 import { NavLink } from "react-router-dom";
-import belen from "../assets/imagen/belen.jpg"
-import bruno from "../assets/imagen/bruno.png"
-import nicole from "../assets/imagen/nicole.png"
-import pablo from "../assets/imagen/pablo.jpg"
-import abel from "../assets/imagen/abel.jpg"
-import larry from "../assets/imagen/larry.jpeg"
-import manu from "../assets/imagen/manu.jpg"
-import franco from "../assets/imagen/franco.jpg"
+import belen from "../assets/imagen/belen.jpg";
+import bruno from "../assets/imagen/bruno.png";
+import nicole from "../assets/imagen/nicole.png";
+import pablo from "../assets/imagen/pablo.jpg";
+import abel from "../assets/imagen/Abel.png";
+import larry from "../assets/imagen/larry.jpeg";
+import manu from "../assets/imagen/manu.jpg";
+import franco from "../assets/imagen/franco.jpg";
+import {
+  Box,
+  VStack,
+  Container,
+  Heading,
+  WrapItem,
+  Avatar,
+  Center,
+  Image,
+  Text,
+  Link,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
 
+const About = () => {
+  return (
+    <Box bg="black">
+      <VStack>
+        <Heading color="yellow" padding="5">
+          ¿QUIENES SOMOS?
+        </Heading>
+      </VStack>
 
-const About = () =>{
-    return (
-    <div className={style.bodyAbout}>
-     <h1 className={style.title}>¿QUIENES SOMOS?</h1>
-     <h2 className={style.parrafo}>
-      SOMOS UN EQUIPO DE TRABAJO QUE SE FUE FORMANDO A LO LARGO DEL BOOTCAMP DE HENRY, 
-      A PESAR DE NO SER DEL MISMO GRUPO DE SUP, LOGRAMOS COINCIDIR Y COORDINAR NUESTRAS
-      IDEAS Y PLASMARLAS PARA LLEGAR AL OBJETIVO EN COMÚN.
-      COMO OBJETIVO SIEMPRE NOS PROPUSIMOS TRABAJAR DE FORMA AMENA, CON RESPETO Y A TRAVES
-      DE ESO PODER DESARROLLAR ALGO QUE SEA CREATIVO, DIVERTIDO Y QUE LO PUEDA UTILIZAR CUALQUIER 
-      PERSONA QUE LE APASIONE EL MUNDO DE LA PROGRAMACION.
-     </h2>
+      <VStack>
+        <Container color="white" textAlign="center" padding="5">
+          SOMOS UN EQUIPO DE TRABAJO QUE SE FUE FORMANDO A LO LARGO DEL BOOTCAMP
+          DE HENRY, A PESAR DE NO SER DEL MISMO GRUPO DE SUP, LOGRAMOS COINCIDIR
+          Y COORDINAR NUESTRAS IDEAS Y PLASMARLAS PARA LLEGAR AL OBJETIVO EN
+          COMÚN. COMO OBJETIVO SIEMPRE NOS PROPUSIMOS TRABAJAR DE FORMA AMENA,
+          CON RESPETO Y A TRAVES DE ESO PODER DESARROLLAR ALGO QUE SEA CREATIVO,
+          DIVERTIDO Y QUE LO PUEDA UTILIZAR CUALQUIER PERSONA QUE LE APASIONE EL
+          MUNDO DE LA PROGRAMACION.
+        </Container>
+      </VStack>
 
-    
-     <div className={style.containicon}>
-        <div>
-        <div className={style.contenedorimagen}>
-        <img src={belen} alt="user"/>
-      </div>
-            <h1 className={style.nameBelen}>María Belén Echazú Higa</h1>
-      <a
-        href="https://www.linkedin.com/in/mar%C3%ADabel%C3%A9nechaz%C3%BAhiga/"
-        target="_blank"
-        rel="noreferrer"
+      <VStack columns={2} spacingX="40px" spacingY="20px">
+        <HStack>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={abel} size="xl" />
+              <Text fontSize="2xl">Abel Yamil Moreno</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/Abelmoreno97" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-      >
-        <img className={style.icon} src={linkedin} alt="linkedin" />
-      </a>
-      <a
-        href="https://github.com/BelenEchazu"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.icon} src={github} alt="github" />
-      </a>
+                <Link
+                  href="https://www.linkedin.com/in/abelmoreno97/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
 
-      </div>
-      
-    </div>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={bruno} size="xl" />
+              <Text fontSize="2xl">Bruno Buglioni</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/brunob43" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-    <div className={style.containicon1}>
-    <div className={style.contenedorimagen}>
-        <img src={bruno} alt="user"/>
-      </div>
-    <h1 className={style.nameBruno}>Bruno Buglioni</h1>
-    <div>
-      <a
-        href="https://www.linkedin.com/in/bruno-buglioni-b0652a263/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.bruno} src={linkedin} alt="linkedin" />
-      </a>
-      <a
-        href="https://github.com/brunob43"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.bruno} src={github} alt="github" />
-      </a>
-      </div>
+                <Link
+                  href="https://www.linkedin.com/in/bruno-buglioni-b0652a263/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
+        </HStack>
+        <HStack>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={nicole} size="xl" />
+              <Text fontSize="2xl">Florencia Braida</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/BelenEchazu" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
+                <Link
+                  href="https://www.linkedin.com/in/nicolea4/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
 
-    </div>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={franco} size="xl" />
+              <Text fontSize="2xl">Franco Frias</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/FrancoFrias23" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-    <div className={style.containicon2}>
-    <div className={style.contenedorimagen}>
-        <img src={nicole} alt="user"/>
-      </div>
-    <h1 className={style.nameNicole}>Florencia Braida</h1>
+                <Link
+                  href="https://www.linkedin.com/in/franco-n-fr%C3%ADas-8a2631216/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
+        </HStack>
 
-    <a
-        href="https://www.linkedin.com/in/nicolea4/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.nicole} src={linkedin} alt="linkedin" />
-      </a>
-      <a
-        href="https://github.com/NicoleA413"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.nicole} src={github} alt="github" />
-    </a>
-    </div>
+        <HStack>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={manu} size="xl" />
+              <Text fontSize="2xl">Manuel Albarracin</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/Roxcon1" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-    <div className={style.containicon3}>
-    <div className={style.contenedorimagen}>
-        <img src={abel} alt="user"/>
-      </div>
-    <h1 className={style.nameAbel}>Abel Yamil Moreno</h1>
-    <a
-         href="https://www.linkedin.com/in/abelmoreno97/"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.abel} src={linkedin} alt="linkedin" />
-       </a>
-       <a
-         href="https://github.com/Abelmoreno97"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.abel} src={github} alt="github" />
-    </a>
-    </div>
+                <Link href="https://github.com/Roxcon1" target="_blank">
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
 
-    <div className={style.containicon4}>
-    <div className={style.contenedorimagen}>
-        <img src={manu} alt="user"/>
-      </div>
-    <h1 className={style.nameManu}>Manuel Albarracín</h1>
-    <a
-         href="https://github.com/Roxcon1"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.manu} src={linkedin} alt="linkedin" />
-       </a>
-       <a
-         href="https://github.com/Roxcon1"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.manu} src={github} alt="github" />
-    </a>
-    </div>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={belen} size="xl" />
+              <Text fontSize="2xl">María Belén Echazú Higa</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/BelenEchazu" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-    <div className={style.containicon5}>
-    <div className={style.contenedorimagen}>
-        <img src={franco} alt="user"/>
-      </div>
-    <h1 className={style.nameFranco}>Franco Frias</h1>
-    <a
-         href="https://www.linkedin.com/in/franco-n-fr%C3%ADas-8a2631216/"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.franco} src={linkedin} alt="linkedin" />
-       </a>
-       <a
-         href="https://github.com/FrancoFrias23"
-         target="_blank"
-         rel="noreferrer"
-       >
-         <img className={style.franco} src={github} alt="github" />
-    </a>
-    </div>
+                <Link
+                  href="https://www.linkedin.com/in/mar%C3%ADabel%C3%A9nechaz%C3%BAhiga/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
+        </HStack>
 
-    <div className={style.containicon6}>
-    <div className={style.contenedorimagen}>
-        <img src={pablo} alt="user"/>
-      </div>
-    <h1 className={style.namePablo}>Pablo Boria</h1>
-    <a
-        href="https://github.com/pabloboria"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.pablo} src={linkedin} alt="linkedin" />
-      </a>
-      <a
-        href="https://github.com/pabloboria"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.pablo} src={github} alt="github" />
-    </a>
-    </div>
+        <HStack>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={larry} size="xl" />
+              <Text fontSize="2xl">Larry Jose Guarenas Arraez</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/Larryjose" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
-    <div className={style.containicon7}>
-    <div className={style.contenedorimagen}>
-        <img src={larry} alt="user"/>
-      </div>
-    <h1 className={style.nameLarry}>Larry Jose Guarenas Arraez</h1>
-    <a
-        href="https://www.linkedin.com/in/larry-jos%C3%A9-guarenas-arraez-100a5a256/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.larry} src={linkedin} alt="linkedin" />
-      </a>
-      <a
-        href="https://github.com/Larryjose"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img className={style.larry} src={github} alt="github" />
-    </a>
-    </div>
+                <Link
+                  href="https://www.linkedin.com/in/larry-jos%C3%A9-guarenas-arraez-100a5a256/"
+                  target="_blank"
+                >
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
 
-    <NavLink to="/contact">
-          <button className={style.button}>CONTACT</button>
-    </NavLink>
+          <WrapItem padding="20">
+            <Center
+              padding="10"
+              borderRadius="10"
+              w="450px"
+              h="100px"
+              bg="rgb(143 112 191)"
+            >
+              <Avatar name="Dan" src={pablo} size="xl" />
+              <Text fontSize="2xl">Pablo Boria</Text>
+              <VStack marginLeft="7">
+                <Link href="https://github.com/pabloboria" target="_blank">
+                  <Image src={github} alt="github" w="10" />
+                </Link>
 
+                <Link href="https://github.com/pabloboria" target="_blank">
+                  <Image src={linkedin} alt="linkedin" w="10" />
+                </Link>
+              </VStack>
+            </Center>
+          </WrapItem>
+        </HStack>
+      </VStack>
 
-   </div>
-
-
-  
-);
+      <HStack justifyContent="center">
+        <NavLink to="/contact">
+          <Button
+            size="md"
+            height="48px"
+            width="200px"
+            border="2px"
+            borderColor="yellow"
+            bg="black"
+            color="white"
+            _hover={{ color: "black", bg: "white" }}
+          >
+            <Text>Contact</Text>
+          </Button>
+        </NavLink>
+      </HStack>
+    </Box>
+  );
 };
 export default About;
-  
