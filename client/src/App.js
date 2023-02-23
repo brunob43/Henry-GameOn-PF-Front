9183319acd49b34ed02d749b89f6a5150469bbc6
Bruno mac <brunob43@outlook.com>
2023-02-23 20:54:24 -0300

    docDetail8

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
 import { Route, Switch} from 'react-router-dom';
 import { Home, About, Contact, Docs, Donation, Games, Register, ShareInfo, GameDetail} from "./views";
-import { DocDetail } from "./views/DocDetail/DocDetail"
+import { DocDetail } from "./views/DocDetail/DocDetail.jsx"
 import NavBar from "./component/NavBar/NavBar"
 import axios from 'axios';
 import { PageNotFound } from './component/PageNotFound/PageNotFound';
