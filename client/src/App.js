78744bea1364ff77eabec7fbd7a61573b209a37e
Abelmoreno97 <abelyamilmoreno@gmail.com>
2023-02-24 10:17:05 -0300

    cambios chicos

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,86 @@
 // import './App.css';
-import { Route, Switch} from 'react-router-dom';
-import { Home, About, Contact, Docs, Donation, Games, Register, ShareInfo, GameDetail} from "./views";
-import NavBar from "./component/NavBar/NavBar"
-import axios from 'axios';
-import { PageNotFound } from './component/PageNotFound/PageNotFound';
+import { Route, Switch } from "react-router-dom";
+import {
+  Home,
+  About,
+  Contact,
+  Docs,
+  Donation,
+  Games,
+  Register,
+  ShareInfo,
+  GameDetail,
+} from "./views";
+import NavBar from "./component/NavBar/NavBar";
+import axios from "axios";
+import { PageNotFound } from "./component/PageNotFound/PageNotFound";
 import Login from "../src/views/Auth/Login/Login";
 import Logout from "../src/views/Auth/Logout/Logout";
-import Profile from "../src/views/Auth/Profile/Profile"
+import Profile from "../src/views/Auth/Profile/Profile";
 // import style from './App.module.
 
-axios.defaults.baseURL= 'https://back-henrygame.up.railway.app';
+axios.defaults.baseURL = "https://back-henrygame.up.railway.app";
 
 function App() {
-    
-    // const location = useLocation();
-    
-    return(
-    
-        <div>
-            <NavBar />
+  // const location = useLocation();
 
-            <Switch>
+  return (
+    <div>
+      <NavBar />
 
-            <Route exact path='/' render ={() => <Home />} />
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
 
-            <Route exact path='/games'>
-                <Games />
-            </Route>
+        <Route exact path="/games">
+          <Games />
+        </Route>
 
-            <Route exact path='/games/:id'>
-                <GameDetail />
-            </Route>
+        <Route exact path="/games/:id">
+          <GameDetail />
+        </Route>
 
-            <Route exact path="/about">
-                <About />
-            </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
 
-            <Route exact path="/contact">
-                <Contact />
-            </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
 
-            <Route exact path="/docs">
-                <Docs />
-            </Route>
+        <Route exact path="/docs">
+          <Docs />
+        </Route>
 
-            <Route exact path="/docs/share">
-                <ShareInfo />
-            </Route>
+        <Route exact path="/docs/share">
+          <ShareInfo />
+        </Route>
 
-            <Route exact path="/register">
-                <Register />
-            </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
 
-            <Route exact path="/donation">
-                <Donation />
-            </Route>
+        <Route exact path="/donation">
+          <Donation />
+        </Route>
 
-            <Route exact path="/login">
-                <Login />
-                <Profile />
-                <Logout />
-            </Route>
-            
-            <Route>
-                <PageNotFound />
-            </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
 
-            </Switch>
+        <Route exact path="/login">
+          <Logout />
+        </Route>
 
-        </div>
+        <Route exact path="/login">
+          <Profile />
+        </Route>
 
-    )
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
