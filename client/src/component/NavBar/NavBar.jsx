1f5699836605d7d077a85224713d1b80727e4f1b
Abelmoreno97 <abelyamilmoreno@gmail.com>
2023-02-23 21:36:27 -0300

    games falta terminar

--- client/src/component/NavBar/NavBar.jsx
+++ client/src/component/NavBar/NavBar.jsx
@@ -1,49 +1,110 @@
 import { NavLink } from "react-router-dom";
 import logonegro from "../../styles/images/HenryLogo.png";
-import logoblanco from "../../styles/images/HenryLogoClaro.webp"
+import logoblanco from "../../styles/images/HenryLogoClaro.webp";
 import React from "react";
-import {useColorMode, Button, HStack, Image} from '@chakra-ui/react'
-import {ColorModeSwitcher} from '../ColorModeSwitcher/ColorModeSwitcher'
+import { useColorMode, Button, HStack, Image } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../ColorModeSwitcher/ColorModeSwitcher";
 
 const NavBar = (props) => {
   const { colorMode } = useColorMode();
   return (
     <div data-bs-theme="light">
-      <HStack flexDirection={["column","column", "row"]} justify='space-around' p='10px' bg={colorMode === "dark" ? "black" : "yellow"}>
-        
-          <a href="https://www.soyhenry.com" target="_blank" rel="noreferrer">
-            <Image p="5px" src={colorMode === "dark" ? logoblanco : logonegro } alt="Henry-Logo" width="240px" height="auto"></Image>
-          </a>
-        
+      <HStack
+        flexDirection={["column", "column", "row"]}
+        justify="space-around"
+        p="10px"
+        bg={colorMode === "dark" ? "black" : "yellow"}
+      >
+        <a href="https://www.soyhenry.com" target="_blank" rel="noreferrer">
+          <Image
+            p="5px"
+            src={colorMode === "dark" ? logoblanco : logonegro}
+            alt="Henry-Logo"
+            width="240px"
+            height="auto"
+          ></Image>
+        </a>
 
-        <HStack flexDirection={["column","column","column","row","row"]}>
+        <HStack flexDirection={["column", "column", "column", "row", "row"]}>
+          <HStack flexDirection={["column", "row", "row", "row", "row"]}>
+            <NavLink to="/">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+              >
+                HOME
+              </Button>
+            </NavLink>
 
-          <HStack 
-          flexDirection={["column","row","row","row","row"]}>
-          <NavLink to="/">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }}>HOME</Button>
-          </NavLink>
+            <NavLink to="/games">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+              >
+                GAMES
+              </Button>
+            </NavLink>
 
-          <NavLink to="/games">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }}>GAMES</Button>
-          </NavLink>
-
-          <NavLink to="/docs">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }}>HENRYDOCS</Button>
-          </NavLink>
+            <NavLink to="/docs">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+              >
+                HENRYDOCS
+              </Button>
+            </NavLink>
           </HStack>
-          <HStack flexDirection={["column","row","row","row","row"]}>
-          <NavLink to="/about">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }}>ABOUT US</Button>
-          </NavLink>
-          <NavLink to="/donation">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }}>DONATION</Button>
-          </NavLink>
-          <NavLink to="/login">
-            <Button variant='ghost' _hover={colorMode === "dark" ? { bg: 'yellow', color: 'black' } : { bg: 'black', color: 'yellow' }} src=" " alt=" ">
-              LOGIN
-            </Button>
-          </NavLink>
+          <HStack flexDirection={["column", "row", "row", "row", "row"]}>
+            <NavLink to="/about">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+              >
+                ABOUT US
+              </Button>
+            </NavLink>
+            <NavLink to="/donation">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+              >
+                DONATION
+              </Button>
+            </NavLink>
+            <NavLink to="/login">
+              <Button
+                variant="ghost"
+                _hover={
+                  colorMode === "dark"
+                    ? { bg: "yellow", color: "black" }
+                    : { bg: "black", color: "yellow" }
+                }
+                src=" "
+                alt=" "
+              >
+                LOGIN
+              </Button>
+            </NavLink>
           </HStack>
           <ColorModeSwitcher />
         </HStack>
--- client/src/views/Games/Games.jsx
+++ client/src/views/Games/Games.jsx
@@ -1,189 +1,236 @@
 import PaginatedGame from "../../component/Paginated/PaginatedGame";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterByDifficultyGames, filterByNameGames, filterByTopicGames, filterByViewsGames, getGames, setCurrentPageGames } from "../../redux/actions";
+import {
+  filterByDifficultyGames,
+  filterByNameGames,
+  filterByTopicGames,
+  filterByViewsGames,
+  getGames,
+  setCurrentPageGames,
+} from "../../redux/actions";
 import style from "./Games.module.css";
 import SearchBarGame from "../../component/SearchBar/SearchBarGame";
 import Error from "../../component/Error/ErrorGames";
-import {Heading, HStack, VStack} from "@chakra-ui/react"
+import { useColorMode, HStack, VStack } from "@chakra-ui/react";
+import bglight from "../../styles/images/fondo_henry_light.jpg"
+import bgdark from "../../styles/images/fondo_henry_dark.png"
+// import fontGame from "../../styles/fonts/I-pixel-u.ttf"
 
-const Games= () =>{
-    const dispatch = useDispatch();
-    const error = useSelector(state => state.errorGames);
-    const allGames = useSelector(state => state.games);
-    const topics = useSelector(state => state.topics);
-    const dificulties = useSelector(state => state.dificulties);
-    // const gamesTopic = useSelector(state => state.gamesTopic)
-    // const gamesDif = useSelector(state => state.gamesDif)
 
-    const [filterSelect, setFilterSelect] = useState({
-        topic: [], 
-        dificulty: [], 
+const Games = () => {
+    const { colorMode } = useColorMode();
+  const dispatch = useDispatch();
+  const error = useSelector((state) => state.errorGames);
+  const allGames = useSelector((state) => state.games);
+  const topics = useSelector((state) => state.topics);
+  const dificulties = useSelector((state) => state.dificulties);
+  // const gamesTopic = useSelector(state => state.gamesTopic)
+  // const gamesDif = useSelector(state => state.gamesDif)
+
+  const [filterSelect, setFilterSelect] = useState({
+    topic: [],
+    dificulty: [],
+  });
+
+  useEffect(() => {
+    if (!allGames.length) {
+      dispatch(getGames());
+    }
+  }, [dispatch, allGames]);
+  //------------------------------------------HANDLERS-------------------------------------------
+
+  let disabledSelectTopic = !!filterSelect.topic.length;
+  let disabledSelectDif = !!filterSelect.dificulty.length;
+
+  const handleFilterTopic = (event) => {
+    const value = event.target.value;
+
+    dispatch(filterByTopicGames(value));
+
+    setFilterSelect({
+      ...filterSelect,
+      topic: [value],
     });
+  };
 
-    useEffect(()=>{
-        if(!allGames.length){
-          dispatch(getGames())
-        }
-      },[dispatch,allGames])
-//------------------------------------------HANDLERS-------------------------------------------
+  const handleFilterDificulty = (event) => {
+    const value = event.target.value;
 
-    let disabledSelectTopic = !(!filterSelect.topic.length);
-    let disabledSelectDif = !(!filterSelect.dificulty.length);
+    dispatch(filterByDifficultyGames(value));
 
-    const handleFilterTopic = (event) => {
-        const value = event.target.value;
+    setFilterSelect({
+      ...filterSelect,
+      dificulty: [value],
+    });
+  };
 
-        dispatch(filterByTopicGames(value));
+  const handleDeleteFilter = (event) => {
+    // const valueDif = filterSelect.dificulty;
+    // console.log(valueDif);
+    // const valueTopic = filterSelect.topic;
+    // console.log(valueTopic);
+    // const valueBtn = event.target.value;
+    // console.log(valueBtn);
 
-        setFilterSelect({
-            ...filterSelect,
-            topic: [value],
-        });
-    };
+    // if(valueDif.length > 0 && valueTopic.length > 0 && valueBtn == valueDif){
+    //     setFilterSelect({
+    //         ...filterSelect,
+    //         dificulty: [],
+    //     });
+    //     dispatch(filterByTopicSelect(valueTopic));
+    // }else if(valueDif.length >0 && valueTopic.length >0 && valueBtn == valueTopic){
+    //     setFilterSelect({
+    //         ...filterSelect,
+    //         topic: [],
+    //     });
+    //     dispatch(filterByDificultySelect(valueDif));
+    // }else{
+    setFilterSelect({
+      topic: [],
+      dificulty: [],
+    });
+    window.location.reload();
+    dispatch(getGames());
+    // }
+  };
 
-    const handleFilterDificulty = (event) => {
-        const value = event.target.value
+  const handleFilterOrder = (event) => {
+    const value = event.target.value;
 
-        dispatch(filterByDifficultyGames(value));
-
-        setFilterSelect({
-            ...filterSelect,
-            dificulty: [value],
-        });
-    };
-
-    const handleDeleteFilter = (event) => {
-        // const valueDif = filterSelect.dificulty;
-        // console.log(valueDif);
-        // const valueTopic = filterSelect.topic;
-        // console.log(valueTopic);
-        // const valueBtn = event.target.value;
-        // console.log(valueBtn);
-
-        // if(valueDif.length > 0 && valueTopic.length > 0 && valueBtn == valueDif){
-        //     setFilterSelect({
-        //         ...filterSelect,
-        //         dificulty: [],
-        //     });
-        //     dispatch(filterByTopicSelect(valueTopic));
-        // }else if(valueDif.length >0 && valueTopic.length >0 && valueBtn == valueTopic){
-        //     setFilterSelect({
-        //         ...filterSelect,
-        //         topic: [],
-        //     });
-        //     dispatch(filterByDificultySelect(valueDif));
-        // }else{
-            setFilterSelect({
-                topic: [],
-                dificulty: [],
-            });
-            window.location.reload();        
-            dispatch(getGames());
-        // } 
+    if (value === "asc" || value === "des") {
+      dispatch(filterByNameGames(value));
+      setCurrentPageGames(1);
+    }
+    if (value === "popular" || value === "unpopular") {
+      dispatch(filterByViewsGames(value));
+      setCurrentPageGames(1);
     }
 
-    const handleFilterOrder = (event) => {
-        const value = event.target.value
-
-        if (value === "asc" || value === "des") {
-
-            dispatch(filterByNameGames(value));
-            setCurrentPageGames(1);
-        }
-        if (value === "popular" || value === "unpopular") {
-
-            dispatch(filterByViewsGames(value));
-            setCurrentPageGames(1);
-        }
-
-        if (value === "default") {
-
-            dispatch(getGames())
-            setCurrentPageGames(1);
-        }
+    if (value === "default") {
+      dispatch(getGames());
+      setCurrentPageGames(1);
     }
+  };
 
-//------------------------------------------VIEW-----------------------------------------------
-    if(error){
-        return(
-            <div className={style.errorcontainer}>
-            <Error/>
-            <div>
-            <button className={style.button} onClick={handleDeleteFilter}>Return to Games</button>
-            </div>
-            </div>
-        )
-    }
+  //------------------------------------------VIEW-----------------------------------------------
+  if (error) {
+    return (
+      <div className={style.errorcontainer}>
+        <Error />
+        <div>
+          <button className={style.button} onClick={handleDeleteFilter}>
+            Return to Games
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-    return(
-        <VStack bg='black' className={style.main}>
-            <HStack >
-             <Heading fontFamily='I-pixel-u' className={style.title}>GAMES</Heading> 
-            </HStack>
+//   .main{
+//     margin-top: 3%;
+//     background-image: url(../../styles/images/background.jpg);
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     color: rgb(255, 255, 0);
+// }
 
-            <div className={style.searchBar}>
-             <SearchBarGame/>   
-            </div>
+  return (
+    <VStack bgImage={colorMode === "dark" ? bgdark : bglight }>
+      <HStack>
+        <h2 className={style.title}>
+          GAMES
+        </h2>
+      </HStack>
 
-            <div className={style.filters}>
-    
-                <div className={style.orderFilter}>
-                    <select onChange={handleFilterOrder} defaultValue="default">
-                        <option value="default">Default</option>
-                        <option value="asc">A-Z</option>
-                        <option value="des">Z-A</option>
-                        <option value="popular">Popular</option>
-                        <option value="unpopular">Unpopular</option>
-                    </select>                        
+      <HStack>
+        <SearchBarGame />
+      </HStack>
+
+      <div className={style.filters}>
+        <div className={style.orderFilter}>
+          <select onChange={handleFilterOrder} defaultValue="default">
+            <option value="default">Default</option>
+            <option value="asc">A-Z</option>
+            <option value="des">Z-A</option>
+            <option value="popular">Popular</option>
+            <option value="unpopular">Unpopular</option>
+          </select>
+        </div>
+
+        <div className={style.filtersContainer}>
+          <div className={style.selectButtons}>
+            <select
+              disabled={disabledSelectTopic}
+              onChange={handleFilterTopic}
+              defaultValue="all"
+            >
+              <option value="all">All Topics</option>
+              {topics.map((topic) => {
+                return (
+                  <option value={topic} key={topic}>
+                    {topic}
+                  </option>
+                );
+              })}
+            </select>
+
+            {filterSelect.topic?.map((topic, index) => {
+              return (
+                <div>
+                  <div key={index}>
+                    <button
+                      value={topic}
+                      name={topic}
+                      key={topic}
+                      onClick={handleDeleteFilter}
+                    >
+                      {topic}
+                    </button>
+                  </div>
                 </div>
+              );
+            })}
+          </div>
+          <div className={style.selectButtons} id="difFilter">
+            <select
+              disabled={disabledSelectDif}
+              onChange={handleFilterDificulty}
+              defaultValue="all"
+            >
+              <option value="all">All Dificulties</option>
+              {dificulties.map((dificulty) => {
+                return (
+                  <option value={dificulty} key={dificulty}>
+                    {dificulty.toUpperCase()}
+                  </option>
+                );
+              })}
+            </select>
 
-                <div className={style.filtersContainer}> 
-                    <div className={style.selectButtons}>
-                        <select disabled={disabledSelectTopic} onChange={handleFilterTopic} defaultValue="all">
-                            <option value="all">All Topics</option>
-                            {topics.map(topic => {
-                            return <option value={topic} key={topic}>{topic}</option>
-                            })}
-                        </select>
-
-                        {filterSelect.topic?.map((topic, index) => {
-                            return (
-                                <div>
-                                    <div key={index}>
-                                        <button value={topic} name={topic} key={topic} onClick={handleDeleteFilter}>{topic}</button>
-                                    </div>
-                                </div>
-                            )
-                        })}                            
-                    </div>
-                    <div className={style.selectButtons} id="difFilter">
-                        <select disabled={disabledSelectDif} onChange={handleFilterDificulty} defaultValue="all">
-                            <option value="all">All Dificulties</option>
-                            {dificulties.map(dificulty => {
-                                return <option value={dificulty} key={dificulty}>{dificulty.toUpperCase()}</option>
-                            })}
-                        </select>
-
-                        {filterSelect.dificulty?.map((dificulty, index) => {
-                            return (
-                                <div>
-                                    <div key={index}>
-                                        <button  value={dificulty} name={dificulty} key={dificulty} onClick={handleDeleteFilter}>{dificulty.toUpperCase()}</button>
-                                    </div>
-                                </div>
-                            )
-                        })}                               
-                    </div>
+            {filterSelect.dificulty?.map((dificulty, index) => {
+              return (
+                <div>
+                  <div key={index}>
+                    <button
+                      value={dificulty}
+                      name={dificulty}
+                      key={dificulty}
+                      onClick={handleDeleteFilter}
+                    >
+                      {dificulty.toUpperCase()}
+                    </button>
+                  </div>
                 </div>
-
-            
-
-            </div>
-            <PaginatedGame />
-        </VStack>
-
-    )
-}
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <PaginatedGame />
+    </VStack>
+  );
+};
 
 export default Games;
