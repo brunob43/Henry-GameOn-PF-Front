eb5426027e316fa12025c0acfbfb9e71f7c0e62c
Bruno mac <brunob43@outlook.com>
2023-02-26 18:40:00 -0300

    sendprofile

--- client/src/component/NavBar/NavBar.jsx
+++ client/src/component/NavBar/NavBar.jsx
@@ -9,6 +9,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { profileCreation } from "../Utils/utils";
+import { sendProfile } from "../../redux/actions";
 
 
 const NavBar = (props) => {
