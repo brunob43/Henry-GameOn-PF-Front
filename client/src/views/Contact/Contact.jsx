fb11c6196cb9cf07183e1614ffdb6f51b4a9226f
UsuarioDeGitHub <belenechazu@outlook.com>
2023-03-03 00:10:06 +0000

    contactydocdetail

--- client/src/views/Contact/Contact.jsx
+++ client/src/views/Contact/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import style from "./Contact.module.css";
 import { postMessage } from "../../redux/actions";
 import { useState } from "react";
-import { VStack, useColorMode, Input, Textarea } from "@chakra-ui/react";
+import { VStack, useColorMode, Input, Textarea ,Text,FormControl,FormLabel, Button} from "@chakra-ui/react";
+import Footer from "../../component/Footer/Footer";
+import foto from "../assets/imagen/contact.jpg";
+
 
 export default function ContactUs() {
   const { colorMode } = useColorMode();
@@ -59,47 +61,52 @@ export default function ContactUs() {
   };
 
   return (
-    <VStack className={style.body}>
-      <VStack
-        mt={["400px", "250px", "200px", "125px", "125px"]}
-        bgColor={colorMode === "dark" ? "gray.200" : "gray.800"}
-        color={colorMode === "dark" ? "gray.800" : "gray.200"}
-        className={style.container}
-      >
-        <div className={style.title}>
-          <h1>Contact Us</h1>
-        </div>
-        <form className={style.form} onSubmit={handleSubmit}>
-          <label>Issue:</label>
+  <>  
+    <VStack bgImage={foto} bgSize="cover" bgPosition="center" h="100vh" >
+    <VStack
+      mt={["450px", "300px", "250px", "175px", "175px"]}
+      bgColor={colorMode === "dark" ? "white" : "black"}
+      color={colorMode === "dark" ? "black" : "white"}
+      borderRadius="15px"
+      h="515"
+    >
+         <Text  p="20px" mt="5px" as="u" >
+          Contact Us
+          </Text>
+        <FormControl letterSpacing={3} p="6" onSubmit={handleSubmit}>
+          <FormLabel mt="-2.5" >Issue:</FormLabel>
           <Input
-            color={colorMode === "dark" ? "gray.200" : "gray.800"}
-            bg={colorMode === "dark" ? "gray.800" : "gray.200"}
+           color={colorMode === "dark" ? "white" : "black"}
+           bg={colorMode === "dark" ? "black" : "white"}
             onChange={handlerInputChange}
             autoComplete="off"
             type="text"
             name="message_issue"
             placeholder="Escribe tu nombre"
             required
+            w="367px"
           />
+
           {error.issue ? (
             <div>
               <p>{error.issue}</p>
             </div>
-          ) : (
-            <br></br>
-          )}
-          <br></br>
+              ) : (
+             <br></br>
+              )}
+              <br></br>
 
-          <label>Email:</label>
+          <FormLabel mt="-2.5" >Email:</FormLabel>
           <Input
-            color={colorMode === "dark" ? "gray.200" : "gray.800"}
-            bg={colorMode === "dark" ? "gray.800" : "gray.200"}
+           color={colorMode === "dark" ? "white" : "black"}
+           bg={colorMode === "dark" ? "black" : "white"}
             onChange={handlerInputChange}
             autoComplete="off"
             type="text"
             name="message_email"
             placeholder="Escribe tu email"
             required
+            w="367px"
           />
           {error.email ? (
             <div>
@@ -111,23 +118,25 @@ export default function ContactUs() {
           <br></br>
 
           {/* <label>Phone:</label>
-        <input
-        autoComplete="off"
-        type= "tel"
-        name="phone"
-        placeholder="Phone"
-        required
-        /> */}
+           <input
+           autoComplete="off"
+           type= "tel"
+          name="phone"
+          placeholder="Phone"
+             required
+          /> */}
 
-          <label>Message:</label>
+          <FormLabel mt="-2.5">Message:</FormLabel>
           <Textarea
-          color={colorMode === "dark" ? "gray.200" : "gray.800"}
-          bg={colorMode === "dark" ? "gray.800" : "gray.200"}
+         color={colorMode === "dark" ? "white" : "black"}
+         bg={colorMode === "dark" ? "black" : "white"}
             onChange={handlerInputChange}
-            className={style.textarea}
+            w="367px"
+            h="80px"
             id="message"
             name="message_content"
             required=""
+          
           ></Textarea>
           {error.content ? (
             <div>
@@ -137,21 +146,35 @@ export default function ContactUs() {
             <br></br>
           )}
           <br></br>
-          <div>
-            <button
+          <VStack>
+            <Button
               disabled={
                 !Object.keys(error).length && input.message_email !== ""
                   ? false
                   : true
               }
-              className={style.button}
-              type="submit"
+              size="md"
+              height="48px"
+              width="100px"
+              border="2px"
+              borderColor={colorMode === "dark" ? "yellow" : "black"}
+              _hover={
+                colorMode === "dark"
+                  ? { color: "black", bg: "yellow" }
+                  : { bg: "black", color: "yellow" }
+              }
+              bg={colorMode === "dark" ? "white" : "yellow"}
             >
               SEND
-            </button>
-          </div>
-        </form>
+            </Button>
+          </VStack>
+        </FormControl>
       </VStack>
-    </VStack>
+      </VStack>
+   
+     
+    
+    </>
+
   );
 }
--- client/src/views/DocDetail/DocDetail.jsx
+++ client/src/views/DocDetail/DocDetail.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import style from "./DocDetail.module.css";
 import { useParams } from "react-router-dom";
-import {VStack, Heading, HStack, Image, Text} from '@chakra-ui/react'
+import {VStack, Heading, HStack, Image, Text, Stack} from '@chakra-ui/react'
 import { getDocDetailFromState, countViewsDocs } from "../../redux/actions";
 
     const DocDetail = () =>{
@@ -13,36 +12,42 @@ import { getDocDetailFromState, countViewsDocs } from "../../redux/actions";
         useEffect(()=>{
             dispatch(getDocDetailFromState(id));
             countViewsDocs(id)
-        }, [dispatch, id])
+           }, [dispatch, id])
         
-        const docDetail = useSelector((state) => state.docDetail);
+           const docDetail = useSelector((state) => state.docDetail);
         
-        const {doc_id} = docDetail;
+          const {doc_id} = docDetail;
 
-     return (
-        <VStack  >
-        {  (id == doc_id) ?
-            <VStack paddingTop="75px">
+           return (
+        <VStack >
+           <Image boxSize='525px' objectFit='cover' w="100%"   alt = {docDetail.doc_name} src = {docDetail.doc_image}></Image>
+            <HStack  >
+            {  (id == doc_id) ?
+          
 
-                <Heading fontSize="60px" mt= {["350px", "200px", "150px", "70px", "70px"]} >{docDetail.doc_name}</Heading>
-                <HStack >
-                    <VStack w="50%" p="20px">
-                <Image borderRadius="15px" alt = {docDetail.doc_name} src = {docDetail.doc_image}></Image>
+            <VStack >
+                <VStack>
+                <Text textAlign="center" fontSize="60px" fontWeight="bold" mt= {["350px", "200px", "150px", "70px", "70px"]} >{docDetail.doc_name}</Text>
+                </VStack>
+                   
+                    <VStack w="70%" p="20px" align="flex-start">
+                  <Text fontSize="34px" fontWeight="bold" >{docDetail.doc_topic}</Text>
+                   <Text fontSize="22px">Author: {docDetail.doc_author}</Text>
+                  <Text fontSize="18px">{docDetail.doc_content}</Text>
                     </VStack>
-                    <VStack w="50%" p="20px">
-                <Text fontSize="34px"  className={style.main}>{docDetail.doc_topic}</Text>
-                <Text fontSize="22px" className={style.main}>{docDetail.doc_author}</Text>
-                <Text fontSize="18px" className={style.main}>{docDetail.doc_content}</Text>
-                    </VStack>
-                </HStack>
-            </VStack>
-            :
+              
+              </VStack>
+              :
              <VStack>
                 <Heading>
                 Loading...
                 </Heading>
              </VStack>
-        }
+              }
+
+
+            
+            </HStack>
      </VStack>
  )
  }
