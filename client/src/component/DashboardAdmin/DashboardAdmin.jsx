6d2f76d503190a1f269f0bda4dfef55681e15393
FrancoFrias23 <lockke2323@gmail.com>
2023-03-03 15:06:41 -0300

    dash

--- client/src/component/DashboardAdmin/DashboardAdmin.jsx
+++ client/src/component/DashboardAdmin/DashboardAdmin.jsx
@@ -442,14 +442,8 @@ const DashboardAdmin =()=>{
              <HStack bg="black" alignItems="flex-start" w="100%" justify="center"></HStack>
              <FormControl alignItems="flex-start" onSubmit={handleDocsSubmit}>
                 <HStack bg="black">
-                <Input
-                w ="250px"
-                type="text"
-                value={input.inputDoc}
-                name="inputDoc"
-                placeholder="Buscar Docs"
-                onChange={handleChange}></Input><Button type="submit">Buscar</Button>
-                <Button onClick={resetDocs}>Reset</Button>
+                
+                <Button></Button>
                 </HStack>
              </FormControl>
              <DataTable
