ea1c30663ee4baac54de8fe29f1b1b765b0997d8
UsuarioDeGitHub <belenechazu@outlook.com>
2023-03-05 22:34:57 +0000

    donation

--- client/src/views/Donation/Donation.jsx
+++ client/src/views/Donation/Donation.jsx
@@ -3,8 +3,8 @@ import coin from "../../styles/images/coin.png";
 import mp from "../../styles/images/Mercado-Pago.jpg";
 import { payment20, payment50, payment100 } from "../../redux/actions";
 import { VStack, useColorMode,Heading, HStack, Text } from "@chakra-ui/react";
-
-
+import bgdark from "../assets/imagen/darkdonation.jpg"
+import bglight from "../assets/imagen/lightdonation.jpg"
 const Donation = () => {
   const { colorMode } = useColorMode();
 
@@ -22,30 +22,31 @@ const Donation = () => {
 
   return (
     <VStack>
+      <VStack  bgImage={colorMode === "dark" ? bgdark : bglight} bgSize="contain" bgPosition="center" w="100%">
       <VStack
-        mt={["250px", "180px", "120px", "110px", "90px"]}
+        mt={["280px", "200px", "140px", "130px", "110px"]}
         mb="50px"
-        bgColor={colorMode === "dark" ? "black" : "yellow"}
+        bgColor={colorMode === "dark" ? "blackAlpha.700" : "whiteAlpha.700"}
         color={colorMode === "dark" ? "yellow" : "black"}
         className={style.cont}
         borderColor={colorMode === "dark" ? "yellow" : "black"}
-        border="5px solid"
-        w={["350px","480px","600px","800px","800px"]}
-        h={["1800px","1600px","1600px","900px","900px"]}
+        border="3px solid"
+        w={["300px","400px","550px","600px","600px"]}
+        h={["1600px","1400px","1400px","700px","700px"]}
       >
-        <Text fontSize={["25px","30px","40px","50px","50px"]} className={style.ttl}>💵DONACIONES💰</Text>
+        <Text fontSize={["15px","25px","30px","40px","40px"]} className={style.ttl}>💵DONACIONES💰</Text>
         <p className={style.msg}>
           Para aportar economicamente al desarrollo del sitio y juntos hacer mas
           contenido y mejoras al espacio HenryGameON
         </p>
-        <Heading fontSize="50px">↓↓fichas↓↓</Heading>
+        <Heading fontSize="25px">↓↓fichas↓↓</Heading>
         <HStack gap="10px" flexDirection={["column","column","column","row","row"]} w="100%" justify="space-around">
           <VStack 
-            bgColor={colorMode === "dark" ? "black" : "yellow"}
+            bgColor={colorMode === "dark" ? "blackAlpha.200" : "whiteAlpha.200"}
             border="2px solid"
             borderColor={colorMode === "dark" ? "yellow" : "black"}
             _hover={colorMode === "dark" ? {bg:"rgb(180, 168, 0);", color:"black"} : {bg:"rgba(27, 27, 0, 0.781);", color:"yellow"}}
-            fontSize="20px"
+            fontSize="16px"
             onClick={pay20}
             className={style.coin}
           >
@@ -54,22 +55,22 @@ const Donation = () => {
             <h1>$20</h1>
           </VStack>
           <VStack 
-          bgColor={colorMode === "dark" ? "black" : "yellow"}
+          bgColor={colorMode === "dark" ? "blackAlpha.200" : "whiteAlpha.200"}
           border="2px solid"
           borderColor={colorMode === "dark" ? "yellow" : "black"}
           _hover={colorMode === "dark" ? {bg:"rgb(180, 168, 0);", color:"black"} : {bg:"rgba(27, 27, 0, 0.781);", color:"yellow"}}
-          fontSize="20px"
+          fontSize="16px"
           onClick={pay50} className={style.coin}>
             <img className={style.imgcoin} src={coin} alt="coin 50"></img>
             <h2>Manón</h2>
             <h1>$50</h1>
           </VStack>
           <VStack 
-          bgColor={colorMode === "dark" ? "black" : "yellow"}
+          bgColor={colorMode === "dark" ? "blackAlpha.200" : "whiteAlpha.200"}
           border="2px solid"
           borderColor={colorMode === "dark" ? "yellow" : "black"}
           _hover={colorMode === "dark" ? {bg:"rgb(180, 168, 0);", color:"black"} : {bg:"rgba(27, 27, 0, 0.781);", color:"yellow"}}
-          fontSize="20px"
+          fontSize="16px"
           onClick={pay100} className={style.coin}>
             <img className={style.imgcoin} src={coin} alt="coin 100"></img>
             <h2>Euforia</h2>
@@ -78,14 +79,18 @@ const Donation = () => {
         </HStack>
         <div className={style.textArea}>
           <br></br>
+         <HStack>
           <p>Las fichas corren en:</p>
-          <img className={style.mp} src={mp} alt="mp"></img>👌🔒
+          <img className={style.mp} src={mp} alt="mp"></img>
+          <Text>👌🔒</Text>
+          </HStack>
         </div>
         <p className={style.textAreaS}>
-          Son aceptadas tarjetas de debito y credito ya sean Visa, MasterCard o
-          American Express. Tambien pago en Rapipago/Pagofacil y mas...
+          Son aceptadas tarjetas de débito y crédito Visa, MasterCard o
+          American Express. También Rapipago/Pagofácil y más...
         </p>
       </VStack>
+    </VStack>
     </VStack>
   );
 };
