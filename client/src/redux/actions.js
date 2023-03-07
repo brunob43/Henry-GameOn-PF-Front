fcf3f122c06cf2d50bbf2923e65257f7918888ae
FrancoFrias23 <lockke2323@gmail.com>
2023-03-07 16:49:41 -0300

    asdasd

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -527,10 +527,12 @@ export async function countViewsDoc (id){
 }
 //--------------------------LIKES-----------------------//
 export async function addLikeGame(id,internal_id) {
+  return async function (dispatch) {
        console.log(id,internal_id,"addLikeGame")
        await axios.put(`/game/like/${id}?like_game=true`);
        await axios.put(`/users/${internal_id}?like_game=true&game_id=${id}`)
-}
+       dispatch({ type: SET_PROFILE, payload: user });
+}}
 export async function removeLikeGame(id,internal_id,aux) {
   console.log(id,internal_id,"addLikeGame")
    await axios.put(`/game/like/${id}`);
