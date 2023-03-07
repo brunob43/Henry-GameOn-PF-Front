99f8de43c27b7a21b0a94fbbe57cfcdd0461fada
UsuarioDeGitHub <belenechazu@outlook.com>
2023-03-07 21:01:30 +0000

    fotodocs

--- client/src/views/DocDetail/DocDetail.jsx
+++ client/src/views/DocDetail/DocDetail.jsx
@@ -49,7 +49,6 @@ const DocDetail = () => {
       <Image
         mt={["162px", "55px", "40px", "40px", "40px"]}
         boxSize="525px"
-        objectFit="cover"
         w="100%"
         alt={docDetail.doc_name}
         src={docDetail.doc_image}
